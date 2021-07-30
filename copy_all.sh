58ee1438709626afb1bd221d30b718bc15ec71e4
Winter Wei <winter@glowing.com>
2021-07-30 16:57:17 +0800

    fix

--- copy_all.sh
+++ copy_all.sh
@@ -19,6 +19,7 @@ echo '>>> copy prepare-rn.gradle/sh to all android repos'
         cp android-build/prepare_rn.gradle ../$repo/
 	cp workflow-templates/android-build.yml ../$repo/.github/workflows
 	cp workflow-templates/update-bom-version.yml ../$repo/.github/workflows
+	cp workflow-templates/update-rn-meta.yml ../$repo/.github/workflows
 	mkdir -p ../$repo/script
 	cp android-build/prepare_rn.sh ../$repo/script
 	cp android-build/make_build_tag.sh ../$repo/script
